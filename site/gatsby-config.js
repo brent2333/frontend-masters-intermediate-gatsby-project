fb65edae385a3d839a513a98470a2e05575af9f4
Brent Lawson <brent.lawson@fleetcor.com>
2023-05-21 19:01:32 -0400

    serverless funcs, auth, config for deploy

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
+    siteMetadata: {
+        title: "Book Club",
+        navItems: [
+            {
+                label: 'Books',
+                path: '/books'
+            },
+            {
+                label: 'Authors',
+                path: '/authors'
+            }
+        ]
+    },
     plugins: [
         'gatsby-plugin-image',
         'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp'
+        'gatsby-transformer-sharp',
+        'gatsby-theme-shared-nav',
+        'gatsby-plugin-netlify'
     ]
 };
